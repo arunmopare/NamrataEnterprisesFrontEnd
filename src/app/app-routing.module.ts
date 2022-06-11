ac14db485d5f4217018dc66d7539b71dacb3819d
Arun Mopare <arunmopare546@gmail.com>
2022-06-11 12:20:10 +0530

    adde join us functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from "./home/home.component";
 import { AboutUsComponent } from "./about-us/about-us.component";
 import { OurServicesComponent } from "./our-services/our-services.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { ContactUsPageComponent } from "./contact-us-page/contact-us-page.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "home", pathMatch: "full" },
@@ -40,6 +41,16 @@ const routes: Routes = [
         "Namrata Enterprises,Shirwal, Labor contractor,Forklift Driver, ITI Welder, Fitter, Electrician, Helper, Gardener, House Keepers",
     },
   },
+  {
+    path: "contact-us",
+    component: ContactUsPageComponent,
+    data: {
+      title: "Contact Us",
+      description: `Namrata Enterprises was established in 2015 into the Labours contractor business. Namrata Enterprises believes in providing the best and most skilled laborers for any kind of client requirement. Namrata Enterprises provide all types of Labours for your Business need. We are a reputed company in Shirwal and have been Serving various companies since 2015. Some of the services we provide are listed below Forklift Driver, ITI Welder, Fitter, Electrician, Helper, Gardener, House Keepers, etc.`,
+      keywords:
+        "Namrata Enterprises,Shirwal, Labor contractor,Forklift Driver, ITI Welder, Fitter, Electrician, Helper, Gardener, House Keepers",
+    },
+  },
   { path: "404", component: PageNotFoundComponent },
   { path: "**", redirectTo: "404" },
 ];
@@ -52,4 +63,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
